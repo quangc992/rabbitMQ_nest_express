149262c735faac725b03ed5d86b9598f23cd4af1
quang <quangngo1500@gmail.com>
2023-04-18 17:59:43 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 import { RabbitMQ } from './ThirdParty/RabbitMQ/RabbitMQPublisher';
-import { ConfigService } from '@nestjs/config';
 
 const app = express();
 const port = 8081;
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
 
-// const configService = new ConfigService();
-// const amqpUrl = configService.get<string>('amqpUrl');
-// const exNameLiveMatch = configService.get<string>('exNameLiveMatch');
-// const exNamePreMatch = configService.get<string>('exNamePreMatch');
-// const exchangeType = configService.get<string>('exTypeFanout');
 const rabbitMQ1Live = new RabbitMQ('amqp://user_broker:sanbul123@103.31.12.63:5672/dataGamev1', 'dataLiveMatch', 'fanout');
 const rabbitMQPre = new RabbitMQ('amqp://user_broker:sanbul123@103.31.12.63:5672/dataGamev1', 'dataPreMatch', 'fanout');
 
+app.get('/LivematchGame/endpoints', async (req, res) => {
+  try {
+    res.json({ success: true, api: 'sanbul : API live data match' });
+  } catch (error) {
+    res.json({ success: false, error });
+  }
+});
+
 app.post('/LivematchGame/endpoints', async (req, res) => {
   try {
+    const startTime = performance.now();
     const contentLength = req.headers['content-length'] / 1024;
     console.log(`body size: ${contentLength.toFixed(2)} kb`);
     rabbitMQ1Live.publish(req.body);
+    console.log(performance.now() - startTime)
     res.json({ success: true });
   } catch (error) {
     res.json({ success: false, error });
@@ -29,9 +33,11 @@ app.post('/LivematchGame/endpoints', async (req, res) => {
 
 app.post('/PrematchGame/endpoints', async (req, res) => {
   try {
+    const startTime = performance.now();
     const contentLength = req.headers['content-length'] / 1024;
     console.log(`body size: ${contentLength.toFixed(2)} kb`);
     rabbitMQPre.publish(req.body);
+    console.log(performance.now() - startTime)
     res.json({ success: true });
   } catch (error) {
     res.json({ success: false, error });
